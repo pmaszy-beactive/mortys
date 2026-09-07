30f18409f84e7e1a04e8f37a076acaa7367cc477
Paul Maszy <paul@beactive.ai>
2026-09-07 15:14:31 +0000

    Vers

--- bumpandpush.sh
+++ bumpandpush.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+git config --global user.email "paul@beactive.ai"
+git config --global user.name "Paul Maszy"
+
 bash scripts/bump-version.sh
 git commit -a -m "Vers"
 git push origin HEAD:main
